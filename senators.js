e3ffbd9fb0c87fe2509aac94bdd05c0d4bf927e7
Daniel O'Flynn <danielpoflynn@gmail.com>
2023-10-11 23:18:50 +0100

    Added blank boxes for count

--- senators.js
+++ senators.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     // fetch data from the JSON file
     async function getData() {
         let data = await fetch("./senators.json");
-        let json = await data.json();
+        let json = data.json();
         return json;
 
     }
@@ -27,12 +27,27 @@ document.addEventListener("DOMContentLoaded", async () => {
         return partyCount;
     }
 
+    // Make boxes for the count of senators by party
+    // Takes object with parties as keys and number of senators as value
+    function makePartyBoxes (partyObj) {
+        for (let key in partyObj) {
+            console.log(key);
+            console.log(partyObj[key]);
+            let el = document.createElement("div");
+            document.getElementById("count-boxes").appendChild(el);
+            
+        }
+    }
+
     // Pull the data from JSON file
     const data = await getData();
     // Names of the different parties
     const parties = getPartyNames(data);
     // object with party as key and count as value
     const partyCount = countSenatorsByParty(data,parties);
+
+    makePartyBoxes(partyCount);
+
 
 
 });
